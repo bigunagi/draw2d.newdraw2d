22a02b56cd1c550fcdf7ff063a1328499a752035
Laurent Le Goff <legoff.laurent@gmail.com>
2011-12-16 10:37:12 +0100

    enhance test && resolve bug on bound detect

--- raster/test/main.go
+++ raster/test/main.go
@@ -2,13 +2,28 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"os"
-	"log"
-	"image/png"
 	"bufio"
 	"code.google.com/p/draw2d/raster"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
+)
+
+var (
+	draws = []Draw{
+		{	
+			"Triangle", 120, 120,
+			[]raster.Polygon{{10, 110, 110, 110, 60, 10}},
+			color.RGBA{0, 0, 0, 0xff},
+		},
+		{
+			"Rectangle", 120, 120,
+			[]raster.Polygon{{10, 10, 110, 10, 110, 110, 10, 110}},
+			color.RGBA{0, 0, 0, 0xff},
+		},
+	}
 )
 
 func savepng(filePath string, m image.Image) {
@@ -30,26 +45,24 @@ func savepng(filePath string, m image.Image) {
 		os.Exit(1)
 	}
 }
-type Figure struct {
-	name string
-	polygon raster.Polygon
-	color color.RGBA
+
+type Draw struct {
+	Name          string
+	Width, Height int
+	Polygons      []raster.Polygon
+	Color         color.RGBA
 }
 
-func (f Figure)save(img *image.RGBA) {
-	tr := [6]float64{1, 0, 0, 1, 0, 0}
-	r := raster.NewRasterizer8BitsSample(200, 200)
-	r.RenderEvenOdd(img, &(f.color), f.polygon, tr)
-	savepng(f.name + ".png", img)
+func (d Draw) save() {
+	tr := [6]float64{1, 0, 0, 1, 0, 0} // identity matrix
+	r := raster.NewRasterizer8BitsSample(d.Width, d.Height)
+	img := image.NewRGBA(image.Rect(0, 0, d.Width, d.Height))
+	r.RenderEvenOdd(img, &(d.Color), d.Polygons, tr)
+	savepng("_test" + d.Name+".png", img)
 }
 
 func main() {
-	figure := Figure{"triangle",  
-			raster.Polygon{10, 110, 110, 110, 60, 10},
-			color.RGBA{0, 0, 0, 0xff}}
-	figure.save(image.NewRGBA(image.Rect(0, 0, 200, 200)))
-	figure = Figure{"rectangle",  
-			raster.Polygon{10, 10, 110, 10, 110, 110, 10, 110},
-			color.RGBA{0, 0, 0, 0xff}}
-	figure.save(image.NewRGBA(image.Rect(0, 0, 200, 200)))
+	for _, d := range draws {
+		d.save()
+	}
 }
